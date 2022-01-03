39b85c1085b44c9f7e98b4bb1a080eaa335f7475
deephigh <kang9kang9@gmail.com>
2022-01-03 19:39:22 +0900

    3min timer

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,62 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <span>{{time}}</span>
+    <br>
+    <button 
+    @click="test">인증번호 전송
+    </button>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import moment from 'moment'
+
+const startTime = '00:03:00'
+const mmssFormat = 'mm:ss'
+const timeFormat = 'hh:mm:ss'
+
+// const a = {
+//   name: '1',  
+//   data: () => {
+//     console.log(this)
+//     return 'a'
+//   },
+//   data2 () {
+//     console.log(this)
+//     return 'b'
+//   }
+// }
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+  data() {
+    return {
+      time: moment(startTime, timeFormat).format(mmssFormat),
+      timer_instance: null,
+      timer_restart: true
+    }
+  },
+  methods: {
+    test() {
+      if (this.timer_instance) {
+        clearInterval(this.timer_instance)
+        this.timer_restart = !this.timer_restart
+      }
+      this.timer_instance = setInterval(this.min, 1000) 
+
+      if (!this.timer_restart) {
+        this.time = moment(startTime, timeFormat).format(mmssFormat)
+        clearInterval(this.timer_instance)
+      }
+    },
+    min() {
+      if (this.time !== "00:00") {
+        this.time = moment(this.time, mmssFormat).subtract(1, "second").format(mmssFormat)
+        console.log(this.timer_restart)
+      } else {
+        document.body.style.backgroundColor = "red"
+      }
+    }
   }
 }
 </script>
@@ -25,4 +70,15 @@ export default {
   color: #2c3e50;
   margin-top: 60px;
 }
+#app span{
+  font-size: 50px;
+}
+
+.go{
+  color:green;
+}
+.stop{
+  color:red
+}
+
 </style>
